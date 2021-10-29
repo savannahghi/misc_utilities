974c711430c646a8fbb24dcdfd1396a1403c764d
Kevin Maiyo <47208315+maiyo01@users.noreply.github.com>
2021-10-29 15:58:47 +0300

    fix: refresh token manager (#23)

--- lib/src/refresh_token_manager.dart
+++ lib/src/refresh_token_manager.dart
@@ -33,20 +33,11 @@ class RefreshTokenManger {
     return this;
   }
 
-  ///if user logs in after expiry return true
-  bool ifTokenIsAfterExpiry(DateTime parsed) {
-    final Duration _afterCurrentTime = parsed.difference(DateTime.now());
-    if (_afterCurrentTime.inSeconds <= 0) {
-      return true;
-    }
-    return false;
-  }
-
   ///if user logs in when token is <= 10minutes to expiry return true
   bool ifTokenIsApproachingExpiry(DateTime parsed) {
-    final Duration _closeToCurrentTime = DateTime.now().difference(parsed);
+    final Duration _remainingTime = DateTime.now().difference(parsed);
     // ten minutes (600 seconds)
-    if (_closeToCurrentTime.inSeconds >= -600) {
+    if (_remainingTime.inSeconds < 600) {
       return true;
     }
     return false;
@@ -55,9 +46,8 @@ class RefreshTokenManger {
   /// [checkExpireValidity] for checking whether the expire time is valid.
   /// Recommended to be called just before the app draws its first widget in main.dart
   bool checkExpireValidity(String? expireAt) {
-    final DateTime _parsed = DateTime.parse(expireAt!);
-    if (this.ifTokenIsAfterExpiry(_parsed) == true ||
-        this.ifTokenIsApproachingExpiry(_parsed) == true) {
+    final DateTime _expireTime = DateTime.parse(expireAt!);
+    if (this.ifTokenIsApproachingExpiry(_expireTime)) {
       return false;
     }
     return true;
@@ -68,29 +58,18 @@ class RefreshTokenManger {
   void reset() {
     try {
       // this is the time from login or retrieved from state store as string
-      final DateTime _parsed = DateTime.parse(this._expireTime.value);
-      // determine if the parsed time is after the expiry time
-      if (this.ifTokenIsAfterExpiry(_parsed)) {
+      final DateTime _expireTime = DateTime.parse(this._expireTime.value);
+      // determine if the parse time is 10 minutes to the expiry time
+      if (this.ifTokenIsApproachingExpiry(_expireTime)) {
         return this.listen.add(true);
       }
 
-      // determine if the parse time is 7 minutes to the expiry time
-      if (this.ifTokenIsApproachingExpiry(_parsed)) {
-        return this.listen.add(true);
-      }
-
-      // refresh 15 minutes before token expires
-      final DateTime _threshold = _parsed.subtract(const Duration(minutes: 15));
+      // refresh 5 minutes before token expires
+      final DateTime _threshold =
+          _expireTime.subtract(const Duration(minutes: 5));
       final Duration _duration = _threshold.difference(DateTime.now());
       if (_duration.inSeconds <= 0) {
-        final Duration _duration = _parsed.difference(DateTime.now());
-        timer = Timer(Duration(seconds: _duration.inSeconds), () {
-          this.listen.add(true);
-        });
-      } else {
-        timer = Timer(Duration(seconds: _duration.inSeconds), () {
-          this.listen.add(true);
-        });
+        this.listen.add(true);
       }
 
       return;
